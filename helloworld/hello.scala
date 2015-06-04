190847116e2bebf9b32e2af703dbe48aeb262a00
Minwoo Kim <minwoo.kim@devsisters.com>
2015-06-04 19:04:08 +0900

    scala hello world!

---
+++ helloworld/hello.scala
@@ -0,0 +1,9 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    var cases = Integer.parseInt(readLine())
+    while (cases >0) {
+      println("Hello, " + readLine() + "! ")
+      cases -= 1
+    }
+  }
+}
